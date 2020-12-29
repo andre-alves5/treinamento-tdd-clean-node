115db8b725fe0a666a8993e6f701e4f0a47157cb
Andre Alves <andrecambridge@gmail.com>
2020-12-28 22:34:21 -0300

    feat: connect to mongodb before app listen

---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://crud_user01:tddcleanpassword01@cluster0.zvqjy.mongodb.net/tdd-clean-node?retryWrites=true&w=majority',
+  port: process.env.PORT || 5050
+}
