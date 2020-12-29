44715e1f1ffcd3e75ead092a8a9ef44bdcfb3c71
Andre Alves <andrecambridge@gmail.com>
2020-12-29 12:10:54 -0300

    test: ensure mongodb reconnects if connection is lost

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -4,19 +4,20 @@ import app from '../config/app'
 import request from 'supertest'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
-    beforeAll(async () => {
-      await MongoHelper.connect(process.env.MONGO_URL)
-    })
-
-    afterAll(async () => {
-      await MongoHelper.disconnect()
-    })
-
-    beforeEach(async () => {
-      const accountCollection = MongoHelper.getCollection('accounts')
-      await accountCollection.deleteMany({})
-    })
     await request(app)
       .post('/api/signup')
       .send({
