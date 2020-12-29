38e80d17efcb023822a80eb1d9c54ad6c517201b
Andre Alves <andrecambridge@gmail.com>
2020-12-29 13:55:55 -0300

    test: ensure LogController decorator returns same response as Controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -47,4 +47,25 @@ describe('LogController Decorator', () => {
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
+
+  test('Should return the same response as the controller', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Andre Alves',
+        email: 'andre@andre.com',
+        password: '123456'
+      }
+    })
+  })
 })
