ac8d2fbe9e8a048984a5f9e693d9bf4bca6809b1
Andre Alves <andrecambridge@gmail.com>
2021-01-02 15:21:41 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb+srv://crud_user01:tddcleanpassword01@cluster0.zvqjy.mongodb.net/tdd-clean-node?retryWrites=true&w=majority',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'lkw+_%3950'
 }
