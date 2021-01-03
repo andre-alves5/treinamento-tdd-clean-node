357a9cea2ea199bc33ec757f88df2930d3ba0d92
Andre Alves <andrecambridge@gmail.com>
2021-01-02 22:47:06 -0300

    chore: add dockercompose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://crud_user01:tddcleanpassword01@cluster0.zvqjy.mongodb.net/tdd-clean-node?retryWrites=true&w=majority',
+  // mongoUrl: process.env.MONGO_URL || 'mongodb+srv://crud_user01:tddcleanpassword01@cluster0.zvqjy.mongodb.net/tdd-clean-node?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/tdd-clean-node-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'lkw+_%3950'
 }
