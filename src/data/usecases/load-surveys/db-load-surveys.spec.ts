a8bd511b12f379f04664f6538bad8033d6afeb9b
Andre Alves <andrecambridge@gmail.com>
2021-01-13 18:27:49 -0300

    chore: update project dependencies

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -25,7 +25,7 @@ const makeFakeSurveys = (): SurveyModel[] => {
 const makeLoadSurveysRepository = (): LoadSurveysRepository => {
   class LoadSurveysRepositoryStub implements LoadSurveysRepository {
     async loadAll (): Promise<SurveyModel[]> {
-      return makeFakeSurveys()
+      return new Promise(resolve => resolve(makeFakeSurveys()))
     }
   }
   return new LoadSurveysRepositoryStub()
