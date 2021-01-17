a475db4291860f64d3033b452c47776488a05d60
Andre Alves <andrecambridge@gmail.com>
2021-01-17 17:12:14 -0300

    fix: ensure SurveyMongoRepository uses MongoHelper.map in all methods

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -1,7 +1,7 @@
 import { SaveSurveyResultRepository } from '@/data/protocols/db/survey/save-survey-result-repository'
 import { SurveyResultModel } from '@/domain/models/survey-result'
 import { SaveSurveyResultModel } from '@/domain/usecases/save-survey-result'
-import { MongoHelper } from '../../helpers/mongo-helper'
+import { MongoHelper } from '../helpers/mongo-helper'
 
 export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
   async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
