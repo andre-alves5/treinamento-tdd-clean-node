e7620f21c61d7cbdcea92d698edd2080283f1124
Andre Alves <andrecambridge@gmail.com>
2021-01-31 23:23:20 -0300

    refactor: move responsability to load the surveyResult to UseCase

--- src/data/usecases/survey-result/save-survey-result/db-save-survey-result.ts
+++ src/data/usecases/survey-result/save-survey-result/db-save-survey-result.ts
@@ -1,10 +1,16 @@
+import { SaveSurveyResult } from '@/domain/usecases/survey-result/save-survey-result'
+import { LoadSurveyResultRepository } from '../load-survey-result/db-load-survey-result-protocols'
 import { SaveSurveyResultRepository, SurveyResultModel, SaveSurveyResultParams } from './db-save-survey-result-protocols'
 
-export class DbSaveSurveyResult implements SaveSurveyResultRepository {
-  constructor (private readonly saveSurveyResultRepository: SaveSurveyResultRepository) {}
+export class DbSaveSurveyResult implements SaveSurveyResult {
+  constructor (
+    private readonly saveSurveyResultRepository: SaveSurveyResultRepository,
+    private readonly loadSurveyResultRepository: LoadSurveyResultRepository
+  ) {}
 
   async save (data: SaveSurveyResultParams): Promise<SurveyResultModel> {
-    const surveyResult = await this.saveSurveyResultRepository.save(data)
+    await this.saveSurveyResultRepository.save(data)
+    const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(data.surveyId)
     return surveyResult
   }
 }
