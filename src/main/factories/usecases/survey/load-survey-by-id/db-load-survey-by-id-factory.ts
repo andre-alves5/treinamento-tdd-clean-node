dbd915bb8e8a2fbd91fb1569466ed8a783d8d403
Andre Alves <andrecambridge@gmail.com>
2021-02-03 13:15:49 -0300

    feat: ensure load survey route returns 403 w=if no accessToken is provided

--- src/main/factories/usecases/survey/load-survey-by-id/db-load-survey-by-id-factory.ts
+++ src/main/factories/usecases/survey/load-survey-by-id/db-load-survey-by-id-factory.ts
@@ -2,7 +2,7 @@ import { DbLoadSurveyById } from '@/data/usecases/survey/load-survey-by-id/db-lo
 import { LoadSurveyById } from '@/domain/usecases/survey/load-surveys-by-id'
 import { SurveyMongoRepository } from '@/infra/db/mongodb/survey/survey-mongo-repository'
 
-export const makeDbLoadSurveyByID = (): LoadSurveyById => {
+export const makeDbLoadSurveyById = (): LoadSurveyById => {
   const surveyMongoRepository = new SurveyMongoRepository()
   return new DbLoadSurveyById(surveyMongoRepository)
 }
